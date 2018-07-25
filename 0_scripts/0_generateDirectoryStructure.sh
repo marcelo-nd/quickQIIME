6554867c8f13bba05377740ddcc7445d22e23263
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2018-07-25 11:32:02 -0500

    Completed directory structure generation script.
    Added 0_generateDirectoryStructure.sh. Variables are read from user input to name the project folders.

--- 0_scripts/0_generateDirectoryStructure.sh
+++ 0_scripts/0_generateDirectoryStructure.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+project_name=$1
+
+#echo $project_name
+
+project_path=$2
+
+#echo $project_path
+
+mkdir $project_path/$project_name
+
+mkdir $project_path/$project_name/1_sequences $project_path/$project_name/2_results $project_path/$project_name/3_output_files $project_path/$project_name/4_log_files $project_path/$project_name/5_error_files
+
+echo Directory structure generated succesfully at \"$project_path/$project_name\".
--- main.sh
+++ main.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
+echo Type project name \(no spaces\).
 
-./manifestGenerator.sh
+read project_name
+
+echo Enter path.
+
+read project_path
+
+#echo project_path
+
+# Execute script for generating folder structure.
+
+echo Generating directory structure for project \"$project_name\".
+
+./0_scripts/0_generateDirectoryStructure.sh $project_name $project_path
+
+#IMPLEMENT LATER
+
+# Execute script for generating manifest file.
+
+# echo Generating manifest file
+
+# ./0_scripts/1_manifestGenerator.sh
+
+# 
+
+# Read user input for workflow selection.
